a2895bc7c0325e38648bedcf2bea0fb4bae52787
Yehya <40221838+BalderasBa@users.noreply.github.com>
2022-03-11 11:41:31 +0100

    reset project

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-const review = document.querySelectorAll(
-  ".reviews-stats .stat-rating .progress span"
-);
 
-const ourReviews = document.querySelector(".reviews");
-
-window.onscroll = () => {
-  console.log(this.scrollY);
-  console.log(ourReviews.offsetTop);
-  if (this.scrollY >= ourReviews.offsetTop - 150) {
-    review.forEach((e) => {
-      e.style.width = e.dataset.rating;
-    });
-  }
-};
